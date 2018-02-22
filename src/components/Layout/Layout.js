f7c90144c6d256a079e22188fad3933a211a64e3
umut <umut.sevil@vertexinc.com>
2018-02-22 11:34:18 -0500

    initial push

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+/**
+ * Created by usevil on 2/22/18.
+ */
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+
+const layout = (props) => (
+
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
---
+++ src/hoc/Aux.js
@@ -0,0 +1,6 @@
+/**
+ * Created by usevil on 2/22/18.
+ */
+const aux = (props) => props.children;
+
+export default aux;
