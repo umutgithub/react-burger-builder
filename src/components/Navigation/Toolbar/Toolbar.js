1e9211a96ae7935b0b5dac1cfc21446677e2da68
umut <umut.sevil@vertexinc.com>
2018-02-26 08:38:45 -0500

    more styling nav items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,14 +10,12 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
        <div>MENU</div>
-       <div>
-           //<Logo height='80%'/> also can pass height as param
+       <div className ={classes.Logo}>
            <Logo />
        </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
            <NavigationItems />
        </nav>
-
     </header>
 
 );
