5f5ceeb98f453a227df19079601117cbf7af6529
umut <umut.sevil@vertexinc.com>
2018-04-05 17:33:08 -0400

    214p Orders page added

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,3 +1,14 @@
 /**
  * Created by usevil on 3/19/18.
  */
+import React from 'react';
+
+import classes from './Order.css';
+const order = (props) => (
+    <div>
+      <p>Ingrediens: Salad (1)</p>
+      <p>Price: <strong>USD 5.45</strong></p>
+    </div>
+);
+
+export default order;
