78a7e8ad9fc7ab4271027e774c7837fc0a6d8b3f
umut <umut.sevil@vertexinc.com>
2018-04-06 22:00:15 -0400

    fethc /orders from database and error handler added to orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -4,8 +4,37 @@
 
 import React,{ Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/wihtErrorHandler';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                //convert res obj to array
+                for( let key in res.data) {
+                   // this will do that but also want to add id prop to array => fetchedOrders.push(res.data[key])
+                    fetchedOrders.push({
+                       ...res.data[key],
+                       id: key
+                    });
+                }
+
+                this.setState({loading: false, orders: fetchedOrders});
+
+            })
+            .catch(err => {
+                this.setState({loading:false});
+            })
+    }
+
    render() {
        return (
         <div>
@@ -17,4 +46,4 @@ class Orders extends Component {
 
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
