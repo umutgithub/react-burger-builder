f5879ab870089f89cf75f77d145da2d420e86fda
umut <umut.sevil@vertexinc.com>
2018-04-17 15:45:06 -0400

    redux setup

---
+++ src/store/actions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by usevil on 4/17/18.
+ */
+
+//const for methods in BurgerBuilder.js
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
---
+++ src/store/reducer.js
@@ -0,0 +1,17 @@
+/**
+ * Created by usevil on 4/17/18.
+ */
+import * as actionTypes from './actions';
+
+const initialState = {
+     ingredients:null,
+     totalPrice : 4
+
+};
+             // if state is undefined set it to initialState
+const reducer = (state = initialState, action) => {
+
+
+}
+
+export default reducer;
